e9d45a3bde76eff68a8336607c22f419dfc19a25
sukmarizki04 <sukmarizki04@gmail.com>
2025-05-03 14:09:31 +0700

    app changes package

--- app.vue
+++ app.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import About from './pages/About.vue';
+import Home from './pages/home.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
 
 </script>
 
 <template>
-  <div>
-    <!-- <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout> -->
-    <Sidebar />
 
-  </div>
+  <NavbarComponent />
+  <section>
+    <About />
+    <Home />
+  </section>
 </template>
