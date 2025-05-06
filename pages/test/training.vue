cc4f00d741d1ee31bc18461316767d20979548fa
sukmarizki04 <sukmarizki04@gmail.com>
2025-05-06 22:09:53 +0700

    long life learner

---
+++ pages/test/training.vue
@@ -0,0 +1,31 @@
+<script setup>
+import About from '../About.vue';
+import Contact from '../Contact.vue';
+import home from '../home.vue';
+</script>
+
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'wwjwjwjjwjw'
+        }
+    },
+    methods: {
+        reversMessage() {
+            this.message = this.message.split('').reverse().join('')
+        },
+        notify() {
+            alert('Notification is stronger')
+        }
+    }
+}
+</script>
+
+
+<template>
+    <h1></h1>
+
+
+</template>
